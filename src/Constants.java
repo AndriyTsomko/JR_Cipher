be4f7b0ad0ab94fbdfa12e295fbc175d17436172
Andriy Tsomko <andriytsomkoo@gmail.com>
2025-04-06 15:55:27 +0300

    Додав до Constants.java константу - англійський алфавіт в нижньому регістрі

--- src/Constants.java
+++ src/Constants.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Constants {
+    public static final ArrayList<Character> ALPHABET_EN_LOWERCASE = new ArrayList<>(Arrays.asList(
+            'a','b','c','d','e','f','g','h','i','j','k',
+            'l','m','n','o','p','q','r','s','t','u','v',
+            'w','x','y','z'
+    ));
 }
