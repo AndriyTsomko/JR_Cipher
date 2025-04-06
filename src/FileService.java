65e456826b6685820578d0646604714addedb719
Andriy Tsomko <andriytsomkoo@gmail.com>
2025-04-06 16:01:54 +0300

    Додав до класу FileService метод writeFile, який поки працює тільки, тоді коли шифрується текст

--- src/FileService.java
+++ src/FileService.java
@@ -1,10 +1,9 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class FileService {
+
     ArrayList<Character> arrayList = new ArrayList<>();
 
     //Метод буде зчитувати char з файлу й зберігати їх в ArrayList
@@ -23,4 +22,23 @@ public class FileService {
 
         return arrayList;
     }
+
+    public void writeFile(String fileName, ArrayList<Character> arrayList) {
+        //Отримуємо path файла
+        Path path = Path.of(fileName);
+        //Отримуємо папку в якій знаходився текстовий файл
+        File file = new File(path.toFile().getAbsolutePath() + "[ENCRYPTED]");
+        //Отримуємо назву нового файлу(Поки працює тільки, коли шифруємо файл)
+        File createNewFile = new File(String.valueOf(file));
+        //Для запису використовую bufferedWriter і FileWriter, в який передаємо шлях до файлу
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(createNewFile))) {
+            //Пробігаємося по лісту за допомогою foreach і записуємо символи
+            for (Character character : arrayList) {
+                bufferedWriter.write(character.toString());
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing file");
+        }
+    }
+
 }
