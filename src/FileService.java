94268c13dec2760ec2a8b6ca7edb8efcfa064711
Andriy Tsomko <andriytsomkoo@gmail.com>
2025-04-06 18:09:49 +0300

    Додав методи writeFile, writeFileEncrypt, writeFileDecrypt для того, щоб новий файл називався ENCRYPTED чи DECRYPTED в залежності від дії

--- src/FileService.java
+++ src/FileService.java
@@ -23,11 +23,7 @@ public class FileService {
         return arrayList;
     }
 
-    public void writeFile(String fileName, ArrayList<Character> arrayList) {
-        //Отримуємо path файла
-        Path path = Path.of(fileName);
-        //Отримуємо папку в якій знаходився текстовий файл
-        File file = new File(path.toFile().getAbsolutePath() + "[ENCRYPTED]");
+    public void writeFile(File file){
         //Отримуємо назву нового файлу(Поки працює тільки, коли шифруємо файл)
         File createNewFile = new File(String.valueOf(file));
         //Для запису використовую bufferedWriter і FileWriter, в який передаємо шлях до файлу
@@ -39,6 +35,22 @@ public class FileService {
         } catch (IOException e) {
             System.out.println("Error writing file");
         }
+    }
+
+    public void writeFileEncrypt(String fileName, ArrayList<Character> arrayList) {
+        //Отримуємо path файла
+        Path path = Path.of(fileName);
+        //Отримуємо папку в якій знаходився текстовий файл
+        File file = new File(path.toFile().getAbsolutePath() + "[ENCRYPTED]");
+        writeFile(file);
+    }
+
+    public void writeFileDecrypt(String fileName, ArrayList<Character> arrayList) {
+        //Отримуємо path файла
+        Path path = Path.of(fileName);
+        //Отримуємо папку в якій знаходився текстовий файл
+        File file = new File(path.toFile().getAbsolutePath() + "[DECRYPTED]");
+        writeFile(file);
     }
 
 }
