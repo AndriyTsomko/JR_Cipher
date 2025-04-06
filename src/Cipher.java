0e1383d20e73450043f052d2f4a1030d036c7adf
Andriy Tsomko <andriytsomkoo@gmail.com>
2025-04-06 18:13:34 +0300

    Видалив коменти

--- src/Cipher.java
+++ src/Cipher.java
@@ -3,24 +3,19 @@ import java.util.Collections;
 
 public class Cipher {
 
-    //Метод буде шифрувати текст, отримуючи на вхід ліст з символами й ключ(для зсуву)
     public void crypt(ArrayList<Character> cipherText, int key) {
-        //Проходимося по кожному символу в лісті
-        //Символи одразу будемо перезаписувати в ліст
         for (int i = 0; i < cipherText.size(); i++) {
-            //Створюємо тимчасову змінну для зберігання одного символу
+
             char temp = cipherText.get(i);
-            //Індекс елемента в алфавіті
             int index;
-            //Отримуємо індекс елемента в алфавіті, який вже зашифрований
-            int resultIndex = 0;
-            //отримуємо поточний алфавіт
+            int resultIndex;
+
             ArrayList<Character> currentAlphabet = getAlphabetForChar(temp);
-            //Перевіряємо чи алфавіт не null й, чи містить він наш символ
+
             if (currentAlphabet != null && currentAlphabet.contains(temp)) {
-                //Отримуємо індекс елемента
+
                 index = currentAlphabet.indexOf(temp);
-                //використовуємо модуль, щоб коли index + key був більший за size алфавіту не викидувало помилку
+
                 if (key >= 0) {
                     resultIndex = (index + key) % currentAlphabet.size();
                 } else {
@@ -30,7 +25,7 @@ public class Cipher {
                         resultIndex = (index - key) % currentAlphabet.size();
                     }
                 }
-                //Замінюємо символ в списку на зашифрований
+
                 char resultChar = currentAlphabet.get(resultIndex);
                 cipherText.set(i, resultChar);
             }
@@ -38,7 +33,7 @@ public class Cipher {
         }
     }
 
-    //Метод розшифровує текст
+
     public void decrypt(ArrayList<Character> cipherText, int key) {
         for (int i = 0; i < cipherText.size(); i++) {
             char temp = cipherText.get(i);
@@ -63,7 +58,7 @@ public class Cipher {
         }
     }
 
-    //Метод буде отримувати поточний алфавіт, в якому знаходиться символ
+
     public ArrayList<Character> getAlphabetForChar(char c) {
         if (Constants.ALPHABET_EN_UPPERCASE.contains(c)) {
             return Constants.ALPHABET_EN_UPPERCASE;
