579c8bca5a7c3d0933facbf4ed3deb8eaefd97ad
Andriy Tsomko <andriytsomkoo@gmail.com>
2025-04-07 17:25:34 +0300

    в методі main ініціалізував об'єкт runner і викликав метод run() для запуску програми

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Runner runner = new Runner();
-        runner.test();
+        runner.run();
     }
 }
