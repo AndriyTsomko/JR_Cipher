496f557f93b614f5f95a1e9bf549c2264f2be22a
Andriy Tsomko <andriytsomkoo@gmail.com>
2025-04-07 17:24:59 +0300

    Додав клас Runner для ініціалізації взаємодії з користувачем через CLI

--- src/Runner.java
+++ src/Runner.java
@@ -2,35 +2,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Runner {
-    public void test() {
-        Scanner console = new Scanner(System.in);
-        System.out.println("Please enter the path: ");
-        String path = console.nextLine();
-
-        System.out.println("Please enter the key: ");
-        int key = console.nextInt();
-        console.nextLine();
-
-        System.out.println("[E]ncrypted or [D]ecrypted text: ");
-        String mode = console.nextLine();
-
-        FileService fileControl = new FileService();
-        Cipher cipher = new Cipher();
-        ArrayList<Character> arrayList = fileControl.readFile(path);
-
-        if (mode.equalsIgnoreCase("E")) {
-            cipher.crypt(arrayList, key);
-            fileControl.writeFileEncrypt(path);
-        }
-
-        if (mode.equalsIgnoreCase("D")) {
-            if (key >= 0) {
-                cipher.decrypt(arrayList, key);
-                fileControl.writeFileDecrypt(path);
-            } else {
-                cipher.decrypt(arrayList, key * -1);
-                fileControl.writeFileDecrypt(path);
-            }
-        }
+    public void run() {
+        CLI cli = new CLI();
+        String path = cli.enterThePath();
+        int key = cli.enterTheKey();
+        String mode = cli.enterTheMode();
+        cli.processFile(path, key, mode);
     }
 }
