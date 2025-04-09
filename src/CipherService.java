756d06f5cb2520d4f1dd08532f7004f420ae6f97
Andriy Tsomko <andriytsomkoo@gmail.com>
2025-04-09 20:39:59 +0300

    changed encrypt() and decrypt() methods to work correctly with negative numbers

--- src/CipherService.java
+++ src/CipherService.java
@@ -14,15 +14,10 @@ public class CipherService {
             if (currentAlphabet != null && currentAlphabet.contains(temp)) {
 
                 index = currentAlphabet.indexOf(temp);
+                resultIndex = (index + key) % currentAlphabet.size();
 
-                if (key >= 0) {
-                    resultIndex = (index + key) % currentAlphabet.size();
-                } else {
-                    if (index - key < 0) {
-                        resultIndex = (currentAlphabet.size() + index - key) % currentAlphabet.size();
-                    } else {
-                        resultIndex = (index - key) % currentAlphabet.size();
-                    }
+                while (resultIndex < 0) {
+                    resultIndex += currentAlphabet.size();
                 }
 
                 char resultChar = currentAlphabet.get(resultIndex);
@@ -35,6 +30,7 @@ public class CipherService {
 
     public void decrypt(ArrayList<Character> cipherText, int key) {
         for (int i = 0; i < cipherText.size(); i++) {
+
             char temp = cipherText.get(i);
             int index;
             int resultIndex;
@@ -42,12 +38,12 @@ public class CipherService {
             ArrayList<Character> currentAlphabet = getAlphabetForChar(temp);
 
             if (currentAlphabet != null && currentAlphabet.contains(temp)) {
-                index = currentAlphabet.indexOf(temp);
 
-                if (index - key < 0) {
-                    resultIndex = (currentAlphabet.size() + index - key) % currentAlphabet.size();
-                } else {
-                    resultIndex = (index - key) % currentAlphabet.size();
+                index = currentAlphabet.indexOf(temp);
+                resultIndex = (index - key) % currentAlphabet.size();
+
+                while (resultIndex < 0) {
+                    resultIndex += currentAlphabet.size();
                 }
 
                 char resultChar = currentAlphabet.get(resultIndex);
@@ -57,7 +53,6 @@ public class CipherService {
         }
     }
 
-//Сенс методу getAlphabetForChar() - дізнатися в якому алфавіті міститься символ.
     public ArrayList<Character> getAlphabetForChar(char c) {
         if (Constants.ALPHABET_EN_UPPERCASE.contains(c)) {
             return Constants.ALPHABET_EN_UPPERCASE;
