102d0f18cc4cc8a418fdf514af9c64008c1a2c92
Andriy Tsomko <andriytsomkoo@gmail.com>
2025-04-17 12:54:31 +0300

    Refactor Runner to handle args in run() instead of constructor

--- src/Runner.java
+++ src/Runner.java
@@ -1,15 +1,10 @@
 public class Runner {
-    private final String mode;
-    private final String path;
-    private final int key;
 
-    public Runner(String[] args) {
-        this.mode = args[0];
-        this.path = args[1];
-        this.key = Integer.parseInt(args[2]);
-    }
+    public void run(String[] args) {
+        String mode = args[0];
+        String path = args[1];
+        int key = Integer.parseInt(args[2]);
 
-    public void run() {
         FileProcessor fileProcessor = new FileProcessor();
         fileProcessor.processFile(mode, path, key);
     }
